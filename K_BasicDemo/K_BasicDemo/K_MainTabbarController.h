9a5bdf5bdb131dccb3ddbf0e5bfc9c92cda1b648
kevin <kaiwen.zhang@baidao.com>
2016-09-30 15:16:25 +0800

    1

---
+++ K_BasicDemo/K_BasicDemo/K_MainTabbarController.h
@@ -0,0 +1,13 @@
+//
+//  K_MainTabbarController.h
+//  K_BasicDemo
+//
+//  Created by kevin.zhang on 16/9/30.
+//  Copyright © 2016年 kevin.zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface K_MainTabbarController : UITabBarController
+
+@end
